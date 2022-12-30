a90074a8577da8493935adc228da5514fd379c8d
bhushan kale <bhushankale175@gmail.com>
2022-12-30 22:54:49 +0530

    created project's basic component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './admin/login/login.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductAddComponent } from './product/product-add/product-add.component';
+import { CategoryAddComponent } from './category/category-add/category-add.component';
+import { CategoryListComponent } from './category/category-list/category-list.component';
+import { BrandAddComponent } from './brand/brand-add/brand-add.component';
+import { BrandLisetComponent } from './brand/brand-liset/brand-liset.component';
+import { BrandListComponent } from './brand/brand-list/brand-list.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductListComponent,
+    ProductAddComponent,
+    CategoryAddComponent,
+    CategoryListComponent,
+    BrandAddComponent,
+    BrandLisetComponent,
+    BrandListComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]

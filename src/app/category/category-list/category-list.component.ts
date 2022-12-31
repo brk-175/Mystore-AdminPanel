72c2f104acc14147e5608079e07bbdaec041d263
bhushan kale <bhushankale175@gmail.com>
2022-12-31 18:16:54 +0530

    finished with categories part

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['./category-list.component.css'],
 })
 export class CategoryListComponent {
+  categories = [];
 
+  constructor(
+    private service: CategoryService,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit() {
+    this.getAllCategories();
+  }
+
+  getAllCategories() {
+    this.service.getAllCategories().subscribe((response: any) => {
+      if (response.status == 'success') this.categories = response.data;
+      else this.toastr.error(response.error);
+    });
+  }
 }
--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  url = 'http://localhost:7000/category';
+  httpOptions = {
+    headers: new HttpHeaders({
+      token: sessionStorage['token'],
+    }),
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllCategories() {
+    return this.http.get(this.url, this.httpOptions);
+  }
 }

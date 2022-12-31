a26bda88a0406bba5fb41ff4c80ee908b8307fc4
bhushan kale <bhushankale175@gmail.com>
2022-12-31 20:10:44 +0530

    added brand functionalites

--- src/app/brand/brand-list/brand-list.component.ts
+++ src/app/brand/brand-list/brand-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { BrandService } from '../brand.service';
 
 @Component({
   selector: 'app-brand-list',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./brand-list.component.css']
 })
 export class BrandListComponent {
+  brands = [];
 
+  constructor(
+    private service: BrandService,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit() {
+    this.getAllBrands();
+  }
+
+  getAllBrands() {
+    this.service.getAllBrands().subscribe((response: any) => {
+      if (response.status == 'success') this.brands = response.data;
+      else this.toastr.error(response.error);
+    });
+  }
 }
--- src/app/brand/brand.service.ts
+++ src/app/brand/brand.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BrandService {
+  url = 'http://localhost:7000/brand';
+  httpOptions = {
+    headers: new HttpHeaders({
+      token: sessionStorage['token'],
+    }),
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllBrands() {
+    return this.http.get(this.url, this.httpOptions);
+  }
 }

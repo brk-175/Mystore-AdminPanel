685b7097cc62691c4c30de3caf6da82ecfc0c295
Bhushan Kale <bhushankale175@gmail.com>
2023-03-12 18:43:38 +0530

    modified some components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,12 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'mystore-adminpanel';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
 
   onLogout() {
     sessionStorage.removeItem('name');
     sessionStorage.removeItem('token');
+    this.toastr.success('Logged Out Successfully!');
     this.router.navigate(['/login']);
   }
 }
